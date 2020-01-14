78ded4dad8cb6a8357dbc763e01c042c07eefba8
aliensmart <kaoua17@gmail.com>
2020-01-14 15:45:44 -0500

    hackerranck practices

---
+++ day8.py
@@ -0,0 +1,20 @@
+import sys
+
+number = int(sys.stdin.readline().strip()) # same as input()
+dict1 = {}
+for i in range(number):
+    name_number = input()
+    name_list = name_number.split(' ')
+    dict1[name_list[0]] = name_list[1]
+
+search_int = sys.stdin.readline().strip()
+
+while search_int:
+    phone_number = dict1.get(search_int)
+    if phone_number:
+        result = search_int + "=" + phone_number
+        print(result)
+    else:
+        print("Not found")
+    search_int = sys.stdin.readline().strip()
+    
